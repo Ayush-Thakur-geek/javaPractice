a7a9277410a3f216513fdb5de303e721305383fd
Ayush Thakur <116510022+Ayush-Thakur-geek@users.noreply.github.com>
2023-08-12 12:11:32 +0530

    Add files via upload

---
+++ Thakur.java
@@ -0,0 +1,69 @@
+package com.ayush;
+
+import java.util.Scanner;
+
+public class Thakur {
+
+    public static void main(String[] args) {
+        //PRIMITIVES
+//        int rollno = 65;     //4 bytes
+//        String name = "Ayush";
+//        char letter = 'A';
+//        float marks = 98.99f;
+//        double largedecimalvalue = 455567.22749;     //8 bytes
+//        long largeintvalue = 879966746888678456L;     //8 bytes
+//        boolean check = false;
+
+
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Please enter some input:");
+//        int rollno = input.nextInt();
+//        int marks = 97;
+//        int correctrollno = marks % 5;
+//        int finalrollno = correctrollno % rollno;
+//        System.out.println("Your Roll No. is - " + correctrollno);
+
+
+//        int a = 234_000_000;
+//        System.out.println(a);
+
+//        System.out.print("Please enter your name - ");
+//        String name = input.nextLine();
+//        System.out.println();
+//        System.out.println("Hello Mr." + name + ", you are an amazing person");
+
+
+//        System.out.print("Please Enter your marks: ");
+//        float marks = input.nextFloat();
+//        System.out.println(marks);
+
+
+//        int num1 = input.nextInt();
+//        int num2 = input.nextInt();
+//        int sum = num1 + num2;
+//        System.out.println("Sum = " + sum);
+
+
+//        float numb = input.nextFloat();
+//        System.out.println(numb);
+
+
+
+        // TYPE CASTING
+//        int numb = (int) (56.56f);
+//        System.out.println(numb);
+
+
+        //AUTOMATIC TYPE PROMOTION IN EXPRESSION
+//        int a = 257;
+//        byte b = (byte) (a); // 257 % 256 = 1
+//        System.out.println(b);
+
+
+        byte a = 40;
+        byte b = 50;
+        byte c = 100;
+        int d = a * b / c;
+        System.out.println(d);
+    }
+}
